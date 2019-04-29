cf89bf4c3e236ee82065b365ee4d4ab58802ed9d
Francisco Adrian Garcia <franciscoadriangarcia1@gmail.com>
2019-04-29 01:10:17 -0600

    Update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -1,4 +1,34 @@
-# Remember to reach out for help after your own due diligence
+# count Alpha chars
+# count char "e"
+# percent of chars are "e" to two decimals
+
+def count_alpha_char(string):
+    alpha_count = 0
+    for char in string:
+        if char.isalpha():
+            alpha_count += 1
+    return alpha_count
+
+def count_e_char(string):
+    e_count = 0
+    e = "e"
+    for char in string:
+        if char == e:
+            e_count +=1
+    return e_count
+    
+def percent_of_char_is_e(e_count, alpha_count):
+    percent = e_count / alpha_count
+    percent = percent * 100
+    percent = "{:.2f}".format(percent)
+    return percent
 
 def analyze_text(text):
     # Your code here
+    alpha_count = count_alpha_char(text)
+
+    e_count = count_e_char(text)
+
+    percent = percent_of_char_is_e(e_count, alpha_count)
+ 
+    return (f"The text contains {alpha_count} alphabetic characters, of which {e_count} ({percent}) are 'e'.")
